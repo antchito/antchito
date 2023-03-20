2c350b698db0553c9cf0fef835ae9758065dbe0a
antchito <117093835+antchito@users.noreply.github.com>
2023-03-20 18:48:56 +0300

    Initial commit

---
+++ src/main/java/com/antchito/antchito/AntchitoApplication.java
@@ -0,0 +1,13 @@
+package com.antchito.antchito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntchitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntchitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antchito/antchito/AntchitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antchito.antchito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntchitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
